2894efa5f66b347570604b65f0b20b02e96418f1
GeorgiStanev92 <georgistanev92@gmail.com>
2021-09-24 13:08:44 +0300

    Add project files.

---
+++ Models/Character.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = "Watsupman";
+        public int HitPoints { get; set; } = 100;
+        public int Strenght { get; set; } = 60;
+        public int Defense { get; set; } = 50;
+        public int Intelligence { get; set; } = 10;
+        public RPGClass Class { get; set; } = RPGClass.Knight;
+    }
+}
