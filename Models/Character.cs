f3692f619668b100bb921ad24de27f9996350d2a
GeorgiStanev92 <georgistanev92@gmail.com>
2021-09-24 17:51:26 +0300

    add user-character relation

--- Models/Character.cs
+++ Models/Character.cs
@@ -14,5 +14,6 @@ namespace dotnet_rpg.Models
         public int Defense { get; set; } = 50;
         public int Intelligence { get; set; } = 10;
         public RPGClass Class { get; set; } = RPGClass.Knight;
+        public User User { get; set; }
     }
 }
