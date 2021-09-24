067d43f8b6f178c19dd9a91ef6387a8e792fbf56
GeorgiStanev92 <georgistanev92@gmail.com>
2021-09-24 19:45:41 +0300

    JWT  Authentication added

--- Controllers/CharacterController.cs
+++ Controllers/CharacterController.cs
@@ -1,6 +1,7 @@
 ﻿using dotnet_rpg.DTOS.Character;
 using dotnet_rpg.Models;
 using dotnet_rpg.Services.CharacterService;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
@@ -9,6 +10,7 @@ using System.Threading.Tasks;
 
 namespace dotnet_rpg.Controllers
 {
+    [Authorize]
     [ApiController]
     [Route("[controller]")]
     public class CharacterController : ControllerBase
