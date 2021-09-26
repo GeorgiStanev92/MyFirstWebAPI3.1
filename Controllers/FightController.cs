c06a6c850c9f2a8c16c27e60f9ddce23e807d88c
GeorgiStanev92 <georgistanev92@gmail.com>
2021-09-26 14:32:21 +0300

    Characters fight with weapons

---
+++ Controllers/FightController.cs
@@ -0,0 +1,30 @@
+﻿using dotnet_rpg.DTOS.Fight;
+using dotnet_rpg.Services.FightService;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Controllers
+{
+    
+    [ApiController]
+    [Route("[controller]")]
+    public class FightController : ControllerBase
+    {
+        private readonly IFightService _fightService;
+
+        public FightController(IFightService fightService)
+        {
+            _fightService = fightService;
+        }
+
+        [HttpPost("Weapon")]
+        public async Task<IActionResult> WeaponAttack(WeaponAttackDto weaponAttack)
+        {
+            return Ok(await _fightService.WeaponAttack(weaponAttack));
+        }
+    }
+}
---
+++ Services/FightService/FightService.cs
@@ -0,0 +1,62 @@
+﻿using dotnet_rpg.Data;
+using dotnet_rpg.DTOS.Fight;
+using dotnet_rpg.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace dotnet_rpg.Services.FightService
+{
+    public class FightService : IFightService
+    {
+        private readonly DataContext _context;
+
+        public FightService(DataContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ServiceResponse<AttackResultDto>> WeaponAttack(WeaponAttackDto request)
+        {
+            ServiceResponse<AttackResultDto> response = new ServiceResponse<AttackResultDto>();
+            try
+            {
+                Character attacker = await _context.Characters
+                    .Include(a => a.Weapon)
+                    .FirstOrDefaultAsync(a => a.Id == request.AttackerId);
+                Character opponent = await _context.Characters
+                    .FirstOrDefaultAsync(o => o.Id == request.OpponentId);
+
+                // calculating damage done with random function and calculating if the damage is enough to defeat opponent
+                int damage = attacker.Weapon.Damage + (new Random().Next(attacker.Strenght));
+                damage -= new Random().Next(opponent.Defense);
+                if (damage > 0)
+                {
+                    opponent.HitPoints -= damage;
+                }
+                if (opponent.HitPoints <= 0)
+                {
+                    response.Message = $"{opponent.Name} has been defeated by {attacker.Name}!";
+                }
+                _context.Characters.Update(opponent);
+                await _context.SaveChangesAsync();
+                response.Data = new AttackResultDto
+                {
+                    Attacker = attacker.Name,
+                    AttackerHP = attacker.HitPoints,
+                    Opponent = opponent.Name,
+                    OpponentHP = opponent.HitPoints,
+                    Damage = damage
+                };
+            }
+            catch (Exception ex)
+            {
+                response.Success = false;
+                response.Message = ex.Message;
+            }
+            return response;
+        }
+    }
+}
